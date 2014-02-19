12d8ba18b492ab2b57872b663d650a3844939c3a
Josiah Webb <jwebb@bluestatedigital.com>
2014-02-18 21:21:39 -0500

    adding VERSION file, version erb template and get block to sinatra app.rb

--- app.rb
+++ app.rb
@@ -7,6 +7,8 @@ require 'sinatra'
 require 'digest/sha1'
 require 'redis'
 require 'yaml'
+require 'net/https'
+require 'uri'
 require 'redis_worker'
 require 'collector/hosts'
 
@@ -14,10 +16,10 @@ include RedisWorker
 include Collector::Hosts
 
 configure do
-  hashed_config_file = YAML.load_file('config/mtarep.yml')
-  set :config_options, hashed_config_file
-
+  set :config_options, YAML.load_file('config/mtarep.yml')
+  set :this_version, IO.read('VERSION').chomp
   set :public_folder, Proc.new { File.join(root, 'vendor') }
+
   set :environment, :production
   set :show_exceptions, true
   enable :sessions, :logging
@@ -37,15 +39,44 @@ end
 before do
   @mta_keys = []
   @redis = redis_connection(settings.config_options['redis_host'])
+  @graph_domains = settings.config_options['graph_domains']
 
   mta_map(settings.config_options['mta_map']).each {|hash| @mta_keys << hash[:ip]}
-  @graph_domains = settings.config_options['graph_domains']
-  date = Time.now.to_s.split[0].gsub('-', '')
+end
 
+get '/' do
+  count = 0
+  begin
+    @assistance_links = settings.config_options['assistance_links']
+    @provider_block_strings = settings.config_options['provider_block_strings']
+    @mta_redis_data_hash = {}
+
+    @mta_keys.each do |ip|
+      @mta_redis_data_hash[ip] = @redis.lrange(ip, 0, -1)
+      new_hash = @mta_redis_data_hash.clone
+
+      @mta_redis_data_hash.each_pair {|ip ,data| new_hash[ip] = Hash[*data.flatten]}
+      @mta_redis_data_hash = new_hash
+    end
+  rescue
+    count += 1
+    sleep 1
+    retry unless count > 5
+  end
+  erb :main
+end
+
+get '/help' do
+  erb :help
+end
+
+get '/graphs' do
   fbl = []
   sent = []
   bounced = []
   expired = []
+
+  date = Time.now.to_s.split[0].gsub('-', '')
 
   def graph_counts(type, graph_domains)
     results = type.inject {|x, y| x.merge(y) {|k, ov, nv| ov + nv}}
@@ -93,42 +124,36 @@ before do
         bounced << {domain => count}
       end
     end
-  end
 
-  @fbl_data = graph_counts(fbl, @graph_domains)
-  @sent_data = graph_counts(sent, @graph_domains)
-  @bounced_data = graph_counts(bounced, @graph_domains)
-  @expired_data = graph_counts(expired, @graph_domains)
-end
-
-get '/' do
-  count = 0
-  begin
-    @assistance_links = settings.config_options['assistance_links']
-    @provider_block_strings = settings.config_options['provider_block_strings']
-    @mta_redis_data_hash = {}
-
-    @mta_keys.each do |ip|
-      @mta_redis_data_hash[ip] = @redis.lrange(ip, 0, -1)
-      new_hash = @mta_redis_data_hash.clone
-
-      @mta_redis_data_hash.each_pair {|ip ,data| new_hash[ip] = Hash[*data.flatten]}
-      @mta_redis_data_hash = new_hash
-    end
-  rescue
-    count += 1
-    sleep 1
-    retry unless count > 5
-  end
-  erb :main
-end
-
-get '/help' do
-  erb :help
-end
-
-get '/graphs' do
   erb :graphs
+end
+
+get '/version' do
+  def latest_version
+    begin
+      uri = URI.parse('https://raw.github.com/phantasm66/mtarep/master/VERSION')
+
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      request = Net::HTTP::Get.new(uri.request_uri)
+      response = http.request(request)
+
+      if response.code.to_s == '200'
+        response.body.to_s
+      else
+        response.code.to_s
+      end
+    rescue SocketError => error
+      return error
+    end
+  end
+
+  @this_version = this_version
+  @latest_version = latest_version
+
+  erb :version
 end
 
 post '/ack' do
